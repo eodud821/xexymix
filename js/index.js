7aaa579a8bc2e947941b51e3588f3da09d485ac9
eodud821 <eodud821@gmail.com>
2021-08-02 13:28:39 +0900

    popup추가

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    var popTarget = $(".popup");
+    var popClose = $(".pop_close");
+    popup($(".callPopup"),popTarget,popClose);
+
     toggle();
     $('.slider').bxSlider({
         speed: 750,
@@ -8,11 +12,18 @@ $(document).ready(function(){
     });
 });
 
-
 function toggle(){
     var $activeTarget = $('header > div > div');
 
     $('header > div > nav > button').click(function(){
         $activeTarget.toggleClass('active');
+    });
+}
+function popup(popOpen,popTarget,popClose){
+    popOpen.click(function(){
+        popTarget.addClass("activate");
+    });
+    popClose.click(function(){
+        popTarget.removeClass("activate");
     });
 }
